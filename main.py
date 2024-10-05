4080efd4af6dbc2771c39541638b24658f7d75b5
George Ghetiu <zcabggh@ucl.ac.uk>
2024-10-05 18:58:20 +0000

    integr fixes

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 from md_parse import get_list_of_users
 from vector.chroma_handler import ChromaHandler
 
-path = "pkms"
+db_handler = ChromaHandler(f"../vector_tings", "test-strings-2")
 
-all_users = get_list_of_users(path)
+if True:
+    path = "pkms"
+    all_users = get_list_of_users(path)
 
-for user in all_users:
-    chunks = user.all_chunks
-    db_handler = ChromaHandler(f"", "test-strings-2")
-    db_handler.add_docs_with_embeddings(
-        [chunk[1] for chunk in chunks],
-        [chunk[0] for chunk in chunks]        
-    )
-
+    for user in all_users:
+        chunks = user.all_chunks
+        texts = [str(chunk[1]) for chunk in chunks]
+        ids = [str(chunk[0]) for chunk in chunks]
+        if not texts or ids:
+            continue
+        db_handler.add_docs_with_embeddings(texts, ids)
+else:
+    print(db_handler.query_collection(["project tasks"], 5))
